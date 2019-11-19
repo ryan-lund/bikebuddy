fd253644b0c83e9a26f5c3b61d2e8bef4973a817
Ryan Lund <ryan.lund@berkeley.edu>
2019-11-19 14:56:24 -0800

    Add toggles for head, tail, and brake light

--- lights.c
+++ lights.c
@@ -7,6 +7,27 @@
 #include "display.h"
 #include "lights.h"
 
+void* init_lights(void*) {
+	// onfigure pins as outputs
+	// 4: left
+	// 5: right
+	// 28: headlight
+	// 29: taillight
+	// 30: brake
+	nrf_gpio_cfg_output(LEFT_PIN);
+	nrf_gpio_cfg_output(RIGHT_PIN); 
+	nrf_gpio_cfg_output(HEAD_PIN);
+	nrf_gpio_cfg_output(TAIL_PIN);
+	nrf_gpio_cfg_output(BRAKE_PIN);
+
+	// insure that all pins are set to low
+	nrf_gpio_pin_clear(LEFT_PIN);
+	nrf_gpio_pin_clear(RIGHT_PIN); 
+	nrf_gpio_pin_clear(HEAD_PIN);
+	nrf_gpio_pin_clear(TAIL_PIN);
+	nrf_gpio_pin_clear(BRAKE_PIN);
+}
+
 void* toggle_left(void*) {
 	return
 }
@@ -16,13 +37,13 @@ void* toggle_right(void*) {
 }
 
 void* toggle_headlight(void*) {
-	return
+	nrf_gpio_pin_toggle(HEAD_PIN);
 }
 
 void* toggle_taillight(void*) {
-	return
+	nrf_gpio_pin_toggle(TAIL_PIN);
 }
 
 void* toggle_brake(void*) {
-	return
+	nrf_gpio_pin_toggle(BRAKE_PIN);
 }
